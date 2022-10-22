5cc7b0ab797fa24efd1be5e70ba0a86bf3806466
Aiden125 <moan125@naver.com>
2022-10-22 22:21:17 +0900

    update

---
+++ src/main/java/com/pro/mbtiletter/MbtiLetterApplication.java
@@ -0,0 +1,13 @@
+package com.pro.mbtiletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtiLetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtiLetterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pro/mbtiletter/MbtiLetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.mbtiletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtiLetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
