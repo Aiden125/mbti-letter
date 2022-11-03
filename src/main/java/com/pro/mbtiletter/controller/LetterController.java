3564a1f926ffad78794e9b703b4d495d384b8190
HeeSeok <mungml1995@gmail.com>
2022-11-04 01:22:41 +0900

    update

--- src/main/java/com/pro/mbtiletter/controller/LetterController.java
+++ src/main/java/com/pro/mbtiletter/controller/LetterController.java
@@ -2,6 +2,7 @@ package com.pro.mbtiletter.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LetterController {
@@ -10,4 +11,19 @@ public class LetterController {
     public String home(){
         return "hello";
     }
+
+    @GetMapping("/main")
+    public String main(){
+        return "main";
+    }
+
+    @GetMapping("/write")
+    public String writeView(){
+        return "write";
+    }
+
+//    @PostMapping("/write")
+//    public String write(){
+//        return "hello";
+//    }
 }
